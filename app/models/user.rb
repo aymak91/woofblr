0cbca71c1c7fa4b3aeac3dd7ef442be47ec74c3d
amakyu <amakyu@gmail.com>
2020-08-18 17:08:53 -0700

    Debugging Heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@
 #  updated_at      :datetime         not null
 #
 class User < ApplicationRecord
-
+    
     validates :username, :email, :password_digest, presence: true
     validates :username, :email, :session_token, uniqueness: true
     validates :password, length: {minimum: 6}, allow_nil: true
--- frontend/woofblr.jsx
+++ frontend/woofblr.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+// this is a comment
+
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
   ReactDOM.render(<h1>Welcome to Woofblr</h1>, root);
